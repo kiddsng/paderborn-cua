a2bdb6ba6e38069fa5fc36d7e8d7152b789d5594
Kidd Sng <sngkidd@outlook.com>
2022-10-14 11:15:25 +0800

    Update models.py
    Removed unused codes, added generate_confusion_matrix function

--- models/models.py
+++ models/models.py
@@ -5,6 +5,11 @@ import numpy as np
 
 from torch.optim import Adam
 
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sn
+import pandas as pd
+
 ### models ###
 
 
@@ -138,149 +143,29 @@ class Discriminator(nn.Module):
 
         return output
 
+# https://christianbernecker.medium.com/how-to-create-a-confusion-matrix-in-pytorch-38d06a7f04b7
+def generate_confusion_matrix(encoder, classifier, dataloader, device):
+    encoder.eval()
+    classifier.eval()
 
-class BaseModel(nn.Module):
-    def __init__(self, input_dim=1, hidden_dim=64, output_dim=3, drop_prob=0.5, lr=1e-4, d_lr=1e-4, batch_size=20, beta1=0.5, beta2=0.9, lambda_rpy=0.03, device='cuda'):
-        super(BaseModel, self).__init__()
+    y_pred = []
+    y_true = []
 
-        # params for configuring the model
-        self.input_dim = input_dim
-        self.hidden_dim = hidden_dim
-        self.output_dim = output_dim
-        self.drop_prob = drop_prob
+    for x, y in dataloader:
+        x, y = x.to(device), y.to(device)
+        pred = classifier(encoder(x))
 
-        # optimizing the model
-        self.lr = lr
-        self.d_lr = d_lr
-        self.batch_size = batch_size
-        self.beta1 = beta1
-        self.beta2 = beta2
-        self.lambda_rpy = lambda_rpy # based on paper
-        self.device = device
+        pred = (torch.max(torch.exp(pred), 1)[1]).data.cpu().numpy()
+        y_pred.extend(pred)
 
-    def print_log(self):
-        print(self.loss_msg)
-        print(self.acc_msg)
+        labels = y.data.cpu().numpy()
+        y_true.extend(labels)
 
-    def train(self, encoder_tgt, discriminator, epoch, dataloader):
-        self.train()
-        
-        self.epoch = epoch
-        losses = {
-            loss: []
-            for loss in self.loss_names
-        }
-        accuracies = {
-            accuracy: []
-            for accuracy in self.accuracy_names
-        }
+    classes = ('Healthy', 'Inner-bearing Damage', 'Outer-bearing Damage')
 
-        for batch_idx, (x_src, y_src, x_tgt, y_tgt, x_rpy, y_rpy) in enumerate(dataloader):
-            self.x_src, self.y_src = x_src.to(self.device), y_src.to(self.device)
-            self.x_tgt, self.y_tgt = x_tgt.to(self.device), y_tgt.to(self.device)
-            self.x_rpy, self.y_rpy = x_rpy.to(self.device), y_rpy.to(self.device)
+    cf_matrix = confusion_matrix(y_true, y_pred)
+    num_labels = np.reshape(np.unique(y_true, return_counts=True)[1], (3, 1))
+    df_cm = pd.DataFrame(cf_matrix / num_labels, index=[i for i in classes], columns=[i for i in classes])
+    plt.figure(figsize = (10,7))
+    sn.heatmap(df_cm, cmap="Blues", annot=True)
 
-            self.optimize_parameters()
-
-            for loss in self.loss_names:
-                losses[loss].append(getattr(self, 'loss_' + loss).detach().item())
-            for accuracy in self.accuracy_names:
-                accuracies[accuracy].append(getattr(self, 'accuracy_' + accuracy).detach().item())
-        
-        self.loss_msg = '[Train][{}] Loss:'.format(epoch)
-        for loss in self.loss_names:
-            self.loss_msg += ' {} {:.3f}'.format(loss, np.mean(losses[loss]))
-        self.acc_msg = '[Train][{}] Accuracy:'.format(epoch)
-        for accuracy in self.accuracy_names:
-            self.acc_msg += ' {} {:.3f}'.format(accuracy, np.mean(accuracies[accuracy]))
-        self.print_log()
-
-    def validate(self, epoch, dataloader):
-        self.eval()
-
-        losses = {
-            loss: []
-            for loss in self.loss_names
-        }
-        accuracies = {
-            accuracy: []
-            for accuracy in self.accuracy_names
-        }
-
-        with torch.no_grad():
-            for batch_idx, (x, y) in enumerate(dataloader):
-                self.x, self.y = x.to(self.device), y.to(self.device)
-
-                self.forward()
-
-                for loss in self.loss_names:
-                    losses[loss].append(getattr(self, 'loss_' + loss).detach().item())
-                for accuracy in self.accuracy_names:
-                    accuracies[accuracy].append(getattr(self, 'accuracy_' + accuracy).detach().item())
-
-                self.loss_msg = '[Val][{}] Loss:'.format(epoch)
-                for loss in self.loss_names:
-                    self.loss_msg += ' {} {:.3f}'.format(loss, np.mean(losses[loss]))
-                self.acc_msg = '[Val][{}] Accuracy:'.format(epoch)
-                for accuracy in self.accuracy_names:
-                    self.acc_msg += ' {} {:.3f}'.format(accuracy, np.mean(accuracies[accuracy]))
-                self.print_log()
-
-    def test(self, epoch, dataloader):
-        self.eval()
-
-        losses = {
-            loss: []
-            for loss in self.loss_names
-        }
-        accuracies = {
-            accuracy: []
-            for accuracy in self.accuracy_names
-        }
-
-        with torch.no_grad():
-            for batch_idx, (x, y) in enumerate(dataloader):
-                self.x, self.y = x.to(self.device), y.to(self.device)
-
-                self.forward()
-
-                for loss in self.loss_names:
-                    losses[loss].append(getattr(self, 'loss_' + loss).detach().item())
-                for accuracy in self.accuracy_names:
-                    accuracies[accuracy].append(getattr(self, 'accuracy_' + accuracy).detach().item())
-
-                self.loss_msg = '[Test][{}] Loss:'.format(epoch)
-                for loss in self.loss_names:
-                    self.loss_msg += ' {} {:.3f}'.format(loss, np.mean(losses[loss]))
-                self.acc_msg = '[Test][{}] Accuracy:'.format(epoch)
-                for accuracy in self.accuracy_names:
-                    self.acc_msg += ' {} {:.3f}'.format(accuracy, np.mean(accuracies[accuracy]))
-                self.print_log()
-
-class SourceOnly(BaseModel):
-    def __init__(self):
-        super(SourceOnly, self).__init__()
-
-        self.criterion = nn.CrossEntropyLoss()
-        self.optimizer = Adam(
-            self.encoder_src.parameters(),
-            lr=self.lr,
-            betas=(self.beta1, self.beta2)
-            )
-        self.loss_names = ['e_pred']
-
-    def forward(self):
-        self.e_src = self.encoder_src(self.x_src)
-        self.pred_src = self.classifier(self.e_src)
-    
-    def backward_e(self):
-        self.loss_e_pred = self.criterion(self.pred_src, self.y_src)
-        self.loss_e = self.loss_e_pred
-        self.loss_e.backward()
-
-    def optimize_parameters(self):
-        self.optimizer.zero_grad()
-        self.forward()
-        self.backward_e()
-        self.optimizer.step()
-        
